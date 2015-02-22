79db57aff2e278f3127d8fb5fa53ed0e1044094d
geekskick <patrickmintram@Patrick-Mintrams-MacBook-Pro.local>
2015-02-22 09:31:02 +0000

    Check if ant is on board added, along with asking the user for the number of moves
    Also some comments to explain stuff

--- main.c
+++ main.c
@@ -9,10 +9,15 @@
 
 #include <stdio.h>
 
+//grid size
 #define HEIGHT 11
 #define WIDTH  11
+
+//Gridsqure fillers
 #define WHITE '.'
 #define BLACK '*'
+
+//Boolean terms
 #define TRUE 1
 #define FALSE 0
 
@@ -28,11 +33,13 @@
             printf("Number of rows in grid is %lu\r\n", N_ROWS(grid));\
 }
 
+//co-ordinates
 struct XY {
     int X;
     int Y;
 };
 
+//ant information
 struct ant{
     struct XY real;
     struct XY temp;
@@ -42,6 +49,7 @@ struct ant{
     char alive;
 };
 
+//gridsquare filler colours
 struct fills{
     char old;
     char new;
@@ -51,49 +59,71 @@ struct fills{
 struct ant enterAnt(char letter);
 void initGrid(char  grid[][WIDTH]);
 void dispGrid(char grid[][WIDTH]);
-int getMoves(void);
+int getMoves(char *exit_flag);
 void updateUserXY(struct ant *ant);
 void makeMove(struct ant *ant, char grid[][WIDTH]);
 void showAntPos(struct ant *ant, char grid[][WIDTH]);
 void moveDir(struct ant *ant);
 void turnLeft(struct ant *ant);
 void dispUserXY(struct ant *ant1,struct ant *ant2,char grid[][WIDTH]);
+void isDeadTest(struct ant *ant);
+void removedMessage(struct ant *ant);
+void endShow(char grid[][WIDTH], struct ant *ant1, struct ant *ant2);
 
-
-//int counter = 0;
 
 int main(int argc, const char * argv[]) {
 
-    //create ants
+    //create ants and their pointers
     struct ant ant1= enterAnt('1'), ant2 = enterAnt('2'), *pAnt1 = &ant1, *pAnt2=&ant2;
-    
-    //colours of grid squares
-    //struct fills fill ={WHITE,BLACK},*pFill = &fill;
-    
-    //creat grid
-    char grid[HEIGHT][WIDTH];
-    
-    //get amount of moves user wants to see
-    int moves = getMoves();
 
-    // initialise grid to all dots
+    char grid[HEIGHT][WIDTH],exit_flag = FALSE;
+
+    // initialise grid to all dots (WHITE)
     initGrid(grid);
     
+    while(1){
+    //get amount of moves user wants to see
+    int moves = getMoves(&exit_flag);
+        
+        if (exit_flag){
+            break;
+        }
+    
+    // makes the desired moves
     for (int i = 1; i <=moves;i++){
         makeMove(pAnt1,grid);
         makeMove(pAnt2,grid);
     }
     
-    dispGrid(grid);
-    dispUserXY(pAnt1,pAnt2,grid);
+    //show the grid and end messages
+    endShow(grid, pAnt1,pAnt2);
+    
+    }
+    
+    //puts ant symbol/number in grid to show where it is at finish
+    showAntPos(pAnt1,grid);
+    showAntPos(pAnt2,grid);
     return 0;
 }
 
-void dispUserXY(struct ant *ant1,struct ant *ant2,char grid[][WIDTH]){
-    printf("USER:\r\nAnt 1 X: %i Y: %i\r\nAnt 2 X: %i Y: %i\r\n",ant1->user.X,ant1->user.Y,ant2->user.X,ant2->user.Y);
-    printf("ARRAY:\r\nAnt 1 X: %i Y: %i\r\nAnt 2 X: %i Y: %i\r\n",ant1->real.X,ant1->real.Y,ant2->real.X,ant2->real.Y);
-    //printf("COUNTER: %i\r\n", counter);
+void endShow(char grid[][WIDTH], struct ant *ant1, struct ant *ant2){
+    //shows the grid
+    dispGrid(grid);
+    
+    //shows the ant co-ordinates
+    dispUserXY(ant1,ant2,grid);
+    
+    //says if ant is dead
+    removedMessage(ant1);
+    removedMessage(ant2);
+}
 
+void dispUserXY(struct ant *ant1,struct ant *ant2,char grid[][WIDTH]){
+    //shows the ant co-ords in (x,y) with indexing from 1
+    printf("USER:\r\nAnt 1 X: %i Y: %i Dir: %c\r\nAnt 2 X: %i Y: %i Dir: %c\r\n",ant1->user.X,ant1->user.Y,ant1->dir,ant2->user.X,ant2->user.Y,ant2->dir);
+    
+    //shows the ant co-ords in (x,y) with indexing from 0
+    printf("ARRAY:\r\nAnt 1 X: %i Y: %i Dir: %c\r\nAnt 2 X: %i Y: %i Dir: %c\r\n",ant1->real.X,ant1->real.Y, ant1->dir,ant2->real.X,ant2->real.Y, ant2->dir);
 }
 
 void moveDir(struct ant *ant){
@@ -112,11 +142,13 @@ void moveDir(struct ant *ant){
             ant->real.X -=1;
             break;
     }
-
+    // change the 'user' co-ordinates to reflect this
     updateUserXY(ant);
 }
 
 void turnLeft(struct ant *ant){
+    
+    //chages direction to the left
     switch(ant->dir){
         case 'T':
             ant->dir = 'L';
@@ -134,6 +166,8 @@ void turnLeft(struct ant *ant){
 }
 
 void turnRight(struct ant *ant){
+    
+    //changes direction to the right
     switch(ant->dir){
         case 'T':
             ant->dir = 'R';
@@ -150,14 +184,37 @@ void turnRight(struct ant *ant){
     }
 }
 
+void isDeadTest(struct ant *ant){
+    
+    //checks ant is still in the grid, if not kill it
+    if(ant->real.Y >=HEIGHT || ant->real.Y <0 || ant->real.X >=HEIGHT || ant->real.X <0){
+        ant->alive=FALSE;
+    }
+}
+
+void removedMessage(struct ant *ant){
+    
+    //if the ant is dead, tell the user
+    if (!ant->alive){
+        printf("Ant %c is DEAD!\r\n",ant->symbol);
+    }
+}
 
 void makeMove(struct ant *ant, char grid[][WIDTH]){
     
     struct fills fill;
     
+    if(ant->alive){
     //moves the ant forward one square based on direction of travel
     moveDir(ant); //already an address so no need to use &
-
+    }
+    
+    //check that the ant is on the grid still
+    isDeadTest(ant);
+    
+    //if ant is on the grid do the colour and direction change
+    if(ant->alive){
+    
     //create old and new filler colours
     fill.old = grid[ant->real.Y][ant->real.X];
     if (fill.old == WHITE){
@@ -186,12 +243,16 @@ void makeMove(struct ant *ant, char grid[][WIDTH]){
         getchar();
     
     }
+    }
     
 }
 
 void showAntPos(struct ant *ant, char grid[][WIDTH]){
-    //insert the ant's symbol to show where it is in the grid
-    grid[ant->real.Y][ant->real.X] = ant->symbol;
+    //insert the ant's symbol to show where it is in the grid only if it's alive
+    
+    if(ant->alive){
+        grid[ant->real.Y][ant->real.X] = ant->symbol;
+    }
 }
 
 void updateUserXY(struct ant *ant){
@@ -200,12 +261,19 @@ void updateUserXY(struct ant *ant){
     ant->user.X = ant->real.X +1;
 }
 
-int getMoves(void){
+int getMoves(char *exit_flag){
+    
     //get the number of moves
     int moves = 0;
-    fflush(stdin);
+
+    fflush(stdin); // flush input buffer to prevent errors
+    
     printf("See the result afer how many moves?\r\n");
     scanf("%i",&moves);
+    
+    if (moves == -1){
+        *exit_flag = TRUE;
+    }
     return moves;
 }
 
@@ -250,10 +318,13 @@ struct ant enterAnt(char letter){
     //flush input buffer to prevent errors in future functions
     fflush(stdin);
     
-    //use realX and Y for the array indexing
+    //use real X and Y for the array indexing
     ant.real.X = ant.user.X-1;
     ant.real.Y = ant.user.Y-1;
+    
+    //do other ant stuff
     ant.symbol = letter;
     ant.alive =TRUE;
+    
     return ant;
 }
